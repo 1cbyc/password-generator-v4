dfe3dbb11b2c988d915d39712bb66a5c5694b5ab
1cbyc <isaacnsisong@gmail.com>
2024-09-21 20:48:05 +0100

    refactoring from c# to c

--- src/main.c
+++ src/main.c
@@ -2,4 +2,15 @@
 #include "generator.h"
 #include "config.h"
 
+#include <stdio.h>
+#include "generator.h"
+#include "config.h"
 
+int main() {
+    Config config = get_user_preferences();
+    generate_password(config.length, config.use_uppercase, config.use_digits, config.use_symbols);
+
+    return 0;
+}
+
+// 
