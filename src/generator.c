b16777360ec36f8f2de7e35c6835f5546816b14f
1cbyc <isaacnsisong@gmail.com>
2024-09-21 20:43:11 +0100

    refactoring from c# to c

--- src/generator.c
+++ src/generator.c
@@ -13,5 +13,5 @@ void generate_password(int length, int use_uppercase, int use_digits, int use_sy
     char password[length + 1];
     char charset[100] = "";
 
-    
+    // that said, i will set up
 
