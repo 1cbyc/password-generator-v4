5d763afbf3fe430cc5c50984daa0e339f68fbafe
1cbyc <isaacnsisong@gmail.com>
2024-09-21 20:46:58 +0100

    refactoring from c# to c

--- src/config.c
+++ src/config.c
@@ -1,3 +1,19 @@
 #include <stdio.h>
 #include "config.h"
 
+#include <stdio.h>
+#include "config.h"
+
+Config get_user_preferences() {
+    Config config;
+    printf("Enter password length: ");
+    scanf("%d", &config.length);
+    printf("Use uppercase letters? (1 for yes, 0 for no): ");
+    scanf("%d", &config.use_uppercase);
+    printf("Use digits? (1 for yes, 0 for no): ");
+    scanf("%d", &config.use_digits);
+    printf("Use symbols? (1 for yes, 0 for no): ");
+    scanf("%d", &config.use_symbols);
+
+    return config;
+}
