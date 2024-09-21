f49bcdcf7949189c8700607a2503ca6ad35a615b
1cbyc <isaacnsisong@gmail.com>
2024-09-21 20:42:42 +0100

    refactoring from c# to c

--- src/generator.c
+++ src/generator.c
@@ -3,3 +3,13 @@
 #include <time.h>
 #include "generator.h"
 
+
+void generate_password(int length, int use_uppercase, int use_digits, int use_symbols) {
+    char lowercase[] = "abcdefghijklmnopqrstuvwxyz";
+    char uppercase[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    char digits[] = "0123456789";
+    char symbols[] = "!@#$%^&*()_+";
+    
+    char password[length + 1];
+    char charset[100] = "";
+
