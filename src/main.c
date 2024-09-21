c0efc913df03cee5e43a3b9c2b9132b271190656
1cbyc <isaacnsisong@gmail.com>
2024-09-21 20:48:24 +0100

    refactoring from c# to c

--- src/main.c
+++ src/main.c
@@ -2,10 +2,6 @@
 #include "generator.h"
 #include "config.h"
 
-#include <stdio.h>
-#include "generator.h"
-#include "config.h"
-
 int main() {
     Config config = get_user_preferences();
     generate_password(config.length, config.use_uppercase, config.use_digits, config.use_symbols);
@@ -13,4 +9,4 @@ int main() {
     return 0;
 }
 
-// 
+// i want to connect everything i have done together at this point, let me even test
